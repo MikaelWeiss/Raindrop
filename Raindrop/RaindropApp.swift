de3c7d993901fca0d7a72c47d6b465fba724b305
Mikael Weiss <Campingmikael@icloud.com>
2020-09-26 13:25:30 -0600

    Initial Commit

---
+++ Raindrop/RaindropApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaindropApp.swift
+//  Raindrop
+//
+//  Created by Mikael Weiss on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct RaindropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
