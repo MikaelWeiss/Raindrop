540483b3fa01155e13475befa9a554f3b09f6e4f
Mikael Weiss <Campingmikael@icloud.com>
2021-01-16 07:42:12 -0700

    Fixed interactor test doubles

--- Raindrop/Presentation/Views/LogEntry/EditEntryInteractorTests.swift
+++ Raindrop/Presentation/Views/LogEntry/EditEntryInteractorTests.swift
@@ -22,6 +22,14 @@ class EditEntryInteractorTests: XCTestCase {
         XCTAssertTrue(presenter.presentUpdateThemeCalled)
     }
     
+    func testFetchItems() {
+        // When
+        interactor.fetchItems()
+        
+        // Then
+        XCTAssertTrue(service.fetchItemsCalled)
+    }
+    
     func testDidChangeValue() {
         // Given
         let request = EditEntry.ValidateValue.Request(value: "Some new value")
@@ -62,28 +70,119 @@ class EditEntryInteractorTests: XCTestCase {
     
     // Either class, or struct with mutating functions
     class EditEntryPresenterDouble: EditEntryPresenting {
-        var value: String?
         var presentUpdateThemeCalled = false
-        var presentPrepareRouteToSheetCalled = false
-        var presentPrepareRouteToOtherSceneCalled = false
-        
-        func presentDidChangeValue(with response: EditEntry.ValidateValue.Response) {
-            value = response.value
-        }
+        var presentDidTapSaveCalled = false
+        var presentFetchItemsResponse: EditEntry.FetchItems.Response?
+        var presentUpdateitemResponse: EditEntry.ValidateItem.Response?
+        var presentErrorError: Error?
+        var presentCheckCanSaveResponse: EditEntry.CheckCanSave.Response?
         
         func presentUpdateTheme() {
             presentUpdateThemeCalled = true
         }
         
-        func presentPrepareRouteToSheet() {
-            presentPrepareRouteToSheetCalled = true
+        func presentFetchItems(with response: EditEntry.FetchItems.Response) {
+            presentFetchItemsResponse = response
         }
         
-        func presentPrepareRouteToOtherScene() {
-            presentPrepareRouteToOtherSceneCalled = true
+        func presentUpdateItem(with response: EditEntry.ValidateItem.Response) {
+            presentUpdateitemResponse = response
         }
+        
+        func presentError(_ error: Error) {
+            presentErrorError = error
+        }
+        
+        func presentDidTapSave() {
+            presentDidTapSaveCalled = true
+        }
+        
+        func presentCheckCanSave(with response: EditEntry.CheckCanSave.Response) {
+            presentCheckCanSaveResponse = response
+        }
+        
     }
     
     class EditEntryServiceDouble: EditEntryService {
+        var items: [Item]!
+        var validateTextFieldInputItem: Item!
+        var validateTextFieldInputError: TestError?
+        var validateTextFieldInputSpy: (input: String, id: UUID)?
+        var validateNumberInputItem: Item!
+        var validateNumberInputError: TestError?
+        var validateNumberInputSpy: (input: String, id: UUID)?
+        var validateDateInputItem: Item!
+        var validateDateInputError: TestError?
+        var validateDateInputSpy: (input: Date, id: UUID)?
+        var validateChecklistInputItem: Item!
+        var validateChecklistInputError: TestError?
+        var validateChecklistInputSpy: (checklistID: UUID, checklistItemID: UUID)?
+        var validateSelectionInputItem: Item!
+        var validateSelectionInputError: TestError?
+        var validateSelectionInputSpy: (selectionID: UUID, selectionItemID: UUID)?
+        var saveConditionMet: Bool!
+        var saveError: TestError?
+        
+        var saveCalled = false
+        
+        func fetchItems() -> [Item] {
+            return items
+        }
+        
+        func validateTextFieldInput(_ input: String, id: UUID) throws -> Item {
+            validateTextFieldInputSpy = (input: input, id: id)
+            if let error = validateTextFieldInputError {
+                throw error
+            } else {
+                return validateTextFieldInputItem
+            }
+        }
+        
+        func validateNumberInput(_ input: String, id: UUID) throws -> Item {
+            validateNumberInputSpy = (input: input, id: id)
+            if let error = validateNumberInputError {
+                throw error
+            } else {
+                return validateNumberInputItem
+            }
+        }
+        
+        func validateDateInput(_ input: Date, id: UUID) throws -> Item {
+            validateDateInputSpy = (input: input, id: id)
+            if let error = validateDateInputError {
+                throw error
+            } else {
+                return validateDateInputItem
+            }
+        }
+        
+        func validateChecklistInput(checklistID: UUID, checklistItemID: UUID) throws -> Item {
+            validateChecklistInputSpy = (checklistID: checklistID, checklistItemID: checklistItemID)
+            if let error = validateChecklistInputError {
+                throw error
+            } else {
+                return validateChecklistInputItem
+            }
+        }
+        
+        func validateSelectionInput(selectionID: UUID, selectionItemID: UUID) throws -> Item {
+            validateSelectionInputSpy = (selectionID: selectionID, selectionItemID: selectionItemID)
+            if let error = validateSelectionInputError {
+                throw error
+            } else {
+                return validateSelectionInputItem
+            }
+        }
+        
+        func canSave() -> Bool {
+            return saveConditionMet
+        }
+        
+        func save() throws {
+            saveCalled = true
+            if let error = saveError {
+                throw error
+            }
+        }
     }
 }
